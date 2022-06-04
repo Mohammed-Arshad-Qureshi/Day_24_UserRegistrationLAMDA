c0118ceb3cdcfa7452a95b835c4a4a9cbd03f227
Arshad <mohammedarshad183@gmail.com>
2022-06-04 08:54:25 +0530

    Added : validation to First Name

--- UserRegistrationLamda/MSTestingUserRegistration/UnitTest1.cs
+++ UserRegistrationLamda/MSTestingUserRegistration/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistrationLamda;
 
 namespace MSTestingUserRegistration
 {
@@ -6,8 +7,24 @@ namespace MSTestingUserRegistration
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public class UserValidation
         {
+            [TestMethod]
+            //Checking for multiple first name
+            [DataRow("Praful", true)]
+            [DataRow("pr", false)]
+            [DataRow("praful", false)]
+            [DataRow("Pr", false)]
+            [DataRow("Pra", true)]
+            public void GivenFirstNameValidation(string firstName, bool expected) // Testing for Firstname Validation
+            {
+                //Arrange
+                SetValidation validation = new SetValidation();
+                //Act
+                bool actual = validation.FirstNameValidation(firstName);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
         }
     }
 }
---
+++ UserRegistrationLamda/UserRegistrationLamda/SetValidation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationLamda
+{
+    public class SetValidation
+    {
+        public bool FirstNameValidation(string fName)
+        {
+            // Regex for first name validation
+            string pattern = "^[A-Z][a-z]{2,}$";
+            if (Regex.IsMatch(fName, pattern))
+            {
+                Console.WriteLine("\nYour first name {0} is valid", fName);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour first name \"{fName}\" is not valid");
+                Console.WriteLine("\nPlease follow naming convention of first name");
+                return false;
+            }
+        }
+    }
+}
