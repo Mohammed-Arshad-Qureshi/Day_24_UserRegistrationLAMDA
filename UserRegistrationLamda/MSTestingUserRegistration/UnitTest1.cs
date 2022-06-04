69452f018788a5fa4379ddfc7cf73aba89a2a0fa
Arshad <mohammedarshad183@gmail.com>
2022-06-04 08:46:16 +0530

    Displaying: Welcome message

---
+++ UserRegistrationLamda/MSTestingUserRegistration/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestingUserRegistration
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UserRegistrationLamda/UserRegistrationLamda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationLamda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registraion");
+        }
+    }
+}
